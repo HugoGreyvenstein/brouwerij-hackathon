686051df643e5652e496f73c3d820dccbcad0457
Hugo <hugo.grey@protonmail.com>
2026-03-27 17:18:15 +0100

    Fixed compilation

--- src/main/kotlin/com/example/brewery/adapter/websocket/BrewingWebSocketHandler.kt
+++ src/main/kotlin/com/example/brewery/adapter/websocket/BrewingWebSocketHandler.kt
@@ -6,7 +6,6 @@ import com.example.brewery.adapter.websocket.mapper.BrewProcessResponseMapper
 import com.example.brewery.domain.model.BrewProcessResult
 import com.example.brewery.domain.model.BrewProcessStage
 import com.example.brewery.domain.port.ForRunningBrewProcess
-import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
@@ -18,6 +17,7 @@ import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
 import reactor.core.publisher.Mono
+import tools.jackson.databind.ObjectMapper
 
 @Component
 class BrewingWebSocketHandler(
