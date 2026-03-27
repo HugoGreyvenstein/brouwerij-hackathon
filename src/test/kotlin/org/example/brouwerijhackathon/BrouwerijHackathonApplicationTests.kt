337ec56caa7d6939b65c2d0a9a10c592666c5191
Hugo <hugo.grey@protonmail.com>
2026-03-27 14:50:01 +0100

    Initial Commit

---
+++ src/test/kotlin/org/example/brouwerijhackathon/BrouwerijHackathonApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.brouwerijhackathon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrouwerijHackathonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
